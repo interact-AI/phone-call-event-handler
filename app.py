22be6c31067cdd5f75fa86d0a6d9093854654a50
Manuel Barrabino <manubarrabino@gmail.com>
2024-05-09 12:53:19 -0300

    entire code

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/echo', methods=['GET'])
+def echo():
+    data = request.args.get('data')
+    return jsonify({'data': data})
+
+if __name__ == '__main__':
+    load_dotenv()
+    port = int(os.getenv('PORT')) if 'PORT' in os.environ else 80
+    print(f"Server running on port {port}")
+    app.run(host='0.0.0.0', port=port)
