c1dc73c6d3e2663b599fc4ad1d5d9f59eea8e60e
Manuel Barrabino <manubarrabino@gmail.com>
2024-05-27 19:35:35 -0300

    arreglo

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -31,15 +31,12 @@ namespace call_handler.Controllers
         }
 
         [HttpPost]
-        public StatusCodeResult Post([FromBody] dynamic request)
+        public async Task<StatusCodeResult> Post([FromBody] dynamic request)
         {
             Console.WriteLine("\n\n------------------------------Called Received------------------------------");
             var callbackUrl = "https://incomingcall-python-api.azurewebsites.net/call";
             var callbackUri = new Uri(callbackUrl);
             var endpoint = ("endpoint=https://voicecallresource.brazil.communication.azure.com/;accesskey=Es26fVjrw3z3vGBQq0lqo4HDlH9QwMqie4mIv2v2VHaKBFzoaXaeM2ljhk68PIqtuB+hl4J2r9GEravehdJGvw==");
-            Console.WriteLine(request);
-            Console.WriteLine("\n\n\n\n");
-            Console.WriteLine(request[0]);
             string requestString = request[0].ToString();
             ResponseWrapper callContextJson = ResponseWrapper.FromJson(requestString);
             var callContext = callContextJson.data.incomingCallContext;
@@ -50,6 +47,9 @@ namespace call_handler.Controllers
             var audioFileUrl = "https://interactaidata.blob.core.windows.net/test/ns2-st10-sca3-sec12.03.wav";
             var audioFileUri = new Uri(audioFileUrl);
             var audioFile = new FileSource(audioFileUri);
+            Console.WriteLine("Esperando a conexion...");
+            await Task.Delay(3000);
+            Console.WriteLine("Va el audio");
             callConnection.GetCallMedia().PlayToAll(audioFile);
             Console.WriteLine("End call");
             return Ok();
