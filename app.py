f615588e62b343db60c8d2c94d32782f20238413
Manuel Barrabino <manubarrabino@gmail.com>
2024-06-06 09:41:54 -0300

    done

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 from flask import Flask, jsonify, request
-from dotenv import load_dotenv
-from azure.communication.callautomation import (
-    CallAutomationClient, 
-    FileSource, 
-    CallConnectionClient,
-    CallRecording,
-)
-from azure.communication.callautomation import FileSource
-import time
 
-print("Setting up azure communication services...")
+
+print("Setting up api...")
 app = Flask(__name__)
-endpoint_url = 'endpoint=https://voicecallresource.brazil.communication.azure.com/;accesskey=Es26fVjrw3z3vGBQq0lqo4HDlH9QwMqie4mIv2v2VHaKBFzoaXaeM2ljhk68PIqtuB+hl4J2r9GEravehdJGvw=='
-callback_url = "https://incomingcall-python-api.azurewebsites.net/call"
-client = CallAutomationClient.from_connection_string(endpoint_url)
-my_file = FileSource(url="https://interactaidata.blob.core.windows.net/test/ns2-st10-sca3-sec12.03.wav")
 
 
 @app.route('/status', methods=['GET'])
@@ -27,26 +15,7 @@ def echo():
     data = request.args.get('data')
     return jsonify({'data': data})
 
-@app.route('/call', methods=['POST'])
-def call_handler():
-    print("\n\n------------------------------Called Received------------------------------")
-    call_context = request.json[0]["data"]["incomingCallContext"]
-    result = client.answer_call(incoming_call_context=call_context, callback_url=callback_url)    
-    call_connection_id = result.call_connection_id
-    call_connection = client.get_call_connection(call_connection_id)
-    print("Start audio file...")
-    call_connection.play_media(my_file, play_to="all")
-    # access call audio stream
-    
-
-    print("End call")
-    
-    return jsonify({'received': "OK"})
-
-
-
-load_dotenv()
-port = 443
+port = 80
 print(f"Server running on port {port}")
 
-app.run(host='0.0.0.0', port=port, ssl_context=('cert.crt', 'private.key'))
+app.run(host='0.0.0.0', port=port)
