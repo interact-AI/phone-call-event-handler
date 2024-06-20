63c272af445caa34b846f90603aa2f198114dcf7
Manuel Barrabino <74431110+Barra001@users.noreply.github.com>
2024-06-20 14:16:22 -0300

    Update app.py
    status y post en 80

--- app.py
+++ app.py
@@ -8,8 +8,21 @@ async def handle_pki_validation(request):
     else:
         return web.Response(text="File not found", status=404)
 
+async def status(req: Request) -> Response:
+    print("Received status request")
+    return Response(status=HTTPStatus.OK, text="Healthy")
+
+async def postInfo(req: Request) -> Response:
+    print("Received info")
+    body = await req.json()
+    print(body)
+    return Response(status=HTTPStatus.OK)
+
 app = web.Application()
 app.router.add_get('/.well-known/pki-validation/38396ED348AFD2CCBC953C4F798FDBFF.txt', handle_pki_validation)
+app.router.add_get("/status", status)
+app.router.add_post("/info", messages)
+
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=8080)
+    web.run_app(app, host="0.0.0.0", port=80)
